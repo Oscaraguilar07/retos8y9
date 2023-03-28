7d485f011b7e84c286b8e55c7977dd5483e0d60e
Oscaraguilar07 <125697206+Oscaraguilar07@users.noreply.github.com>
2023-03-27 19:44:02 -0500

    Add files via upload

---
+++ reto8.py
@@ -0,0 +1,12 @@
+""" construya un programa que solicite las notas a evaluar (libre) en un la 
+materia de programacion con python el valor de cada una de 
+dichas notas debe ser de 0.5 a 5.0 calcule el promedio de dichas de notas
+para obtener la nota final y agregue las siguientes anotaciones segun corresponda
+ 
+ si la nota es menor que 3.0 reprobaste
+ si la nota esta entre 3.0 y 4.0 pasaste raspanado
+ si la nota es mayor que 4.0 aprobaste con buesnos resultados"""
+
+print ("hola ingrese las notas del estudiante: ");
+input(float("ingresa la primera nota: "));
+
